53270407fbc1d3f9340f1cf1590748ba704f76b4
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-06 15:07:03 +0200

    .

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,14 @@
-Meteor.publish("TousAppareils", function(){
-    return Appareils.find();
+Meteor.publish("TousLesObjets", function(){
+    return  [
+        Appareils.Find(),
+        Consommables.Find(),
+        Fournisseurs.Find(),
+        Lieux.Find(),
+        Locaux.Find(),
+        Marques.Find(),
+        Modeles.Find(),
+        TypesMachine.Find(),
+        Villes.Find()
+    ]
+    
 });
