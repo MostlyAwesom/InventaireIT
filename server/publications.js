5e99292112a54f220ae6ef2ad09aedef05bd1954
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-09 10:08:18 +0200

    .

--- server/publications.js
+++ server/publications.js
@@ -15,3 +15,27 @@ Meteor.publish("TousLesObjets", function(){
 Meteor.publish("Fournisseurs", function(){
     return  Fournisseurs.find();
 });
+
+Meteor.publish("Lieux", function(){
+    return  Lieux.find();
+});
+
+Meteor.publish("Locaux", function(){
+    return  Locaux.find();
+});
+
+Meteor.publish("Marques", function(){
+    return  Marques.find();
+});
+
+Meteor.publish("Modeles", function(){
+    return  Modeles.find();
+});
+
+Meteor.publish("TypesMachine", function(){
+    return  TypesMachine.find();
+});
+
+Meteor.publish("Villes", function(){
+    return  Villes.find();
+});
