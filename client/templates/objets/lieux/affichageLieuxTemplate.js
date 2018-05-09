35d3f4273e006cc902026e0fa270ed177e635d8d
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-09 09:50:02 +0200

    .

--- client/templates/objets/lieux/affichageLieuxTemplate.js
+++ client/templates/objets/lieux/affichageLieuxTemplate.js
@@ -1,3 +1,9 @@
+Template.affichageLieuxTemplate.events({
+	'click #delete_btn': function(){
+		Lieux.remove(this._id);	
+	}
+});
+
 Template.affichageLieuxTemplate.helpers({
     ville() {
         return Villes.findOne(this.villeId);
--- client/templates/objets/locaux/affichageLocauxTemplate.js
+++ client/templates/objets/locaux/affichageLocauxTemplate.js
@@ -1,5 +1,14 @@
+Template.affichageLocauxTemplate.events({
+	'click #delete_btn': function(){
+		Locaux.remove(this._id);	
+	}
+});
+
 Template.affichageLocauxTemplate.helpers({
     lieu() {
         return Lieux.findOne(this.lieuId);
+    },
+    ville() {
+        return Villes.findOne(this.lieuId.villeId);
     }
 });
