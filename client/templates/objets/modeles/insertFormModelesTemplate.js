2c3d11a64963b685d3a324b645e5e0277048e9e9
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-14 11:59:14 +0200

    .

--- client/templates/objets/modeles/insertFormModelesTemplate.js
+++ client/templates/objets/modeles/insertFormModelesTemplate.js
@@ -1,7 +1,5 @@
-Template.insertFormModelesTemplate.helpers({
-  collection(){
-    return Modeles;
-  }
+Template.registerHelper("marqueOptions", function() {
+  return Marques.find().map(function (marque) {
+    return {label: marque.name, value: marque._id};
+  });
 });
-
-
