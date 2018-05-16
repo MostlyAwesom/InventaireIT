f31792705e2c768eaee834d244d6c459417b15ca
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-16 13:28:22 +0200

    .

--- client/templates/objets/lieux/lieuxFormsHelpers.js
+++ client/templates/objets/lieux/lieuxFormsHelpers.js
@@ -1,23 +1,7 @@
-Template.insertFormLieuxTemplate.helpers({
-    villesOptions: function () {
-      return Villes.find().map(function (v) {
-        return {label: v.nom, value: v._id};
-      });
-    }
-});
-
 Template.updateFormLieuxTemplate.events({ 
   'click #submitUpdate_btn': function() { 
        Router.go("Lieux");
   } 
-});
-
-Template.updateFormLieuxTemplate.helpers({
-  villesOptions: function () {
-    return Villes.find().map(function (v) {
-      return {label: v.nom, value: v._id};
-    });
-  }
 });
 
 Template.readFormLieuxTemplate.events({ 
@@ -25,11 +9,3 @@ Template.readFormLieuxTemplate.events({
        Router.go("Lieux");
   } 
 }); 
-
-Template.readFormLieuxTemplate.helpers({
-  villesOptions: function () {
-    return Villes.find().map(function (v) {
-      return {label: v.nom, value: v._id};
-    });
-  }
-});
--- client/templates/objets/locaux/locauxFormsHelpers.js
+++ client/templates/objets/locaux/locauxFormsHelpers.js
@@ -1,35 +1,11 @@
-Template.insertFormLocauxTemplate.helpers({
-    lieuxOptions: function () {
-      return Lieux.find().map(function (l) {
-        return {label: l.nom, value: l._id};
-      });
-    }
-});
-
 Template.updateFormLocauxTemplate.events({ 
   'click #submitUpdate_btn': function() { 
        Router.go("Locaux");
   } 
 });
 
-Template.updateFormLocauxTemplate.helpers({
-  lieuxOptions: function () {
-    return Lieux.find().map(function (l) {
-      return {label: l.nom, value: l._id};
-    });
-  }
-});
-
 Template.readFormLocauxTemplate.events({ 
   'click #submitRead_btn': function() { 
        Router.go("Locaux");
   } 
-});
-
-Template.readFormLocauxTemplate.helpers({
-  lieuxOptions: function () {
-    return Lieux.find().map(function (l) {
-      return {label: l.nom, value: l._id};
-    });
-  }
 });
