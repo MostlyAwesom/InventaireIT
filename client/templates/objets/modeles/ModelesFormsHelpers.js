7316c0cf6fee5bc10f4ad9eabab1d8d4beb36f09
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-16 12:36:17 +0200

    .

--- client/templates/objets/modeles/ModelesFormsHelpers.js
+++ client/templates/objets/modeles/ModelesFormsHelpers.js
@@ -1,35 +1,12 @@
-/*
-Template.insertFormModelesTemplate.helpers({
-  marqueOptions: function () {
-    return Marques.find().map(function (m) {
-      return {label: m.nom, value: m._id};
-    });
-  }
-});*/
-
 Template.updateFormModelesTemplate.events({ 
     'click #submitUpdate_btn': function() { 
       Router.go('Modeles');
     } 
 });
 
-Template.updateFormModelesTemplate.helpers({
-  marqueOptions: function () {
-    return Marques.find().map(function (m) {
-      return {label: m.nom, value: m._id};
-    });
-  }
-});
 Template.readFormModelesTemplate.events({ 
   'click #submitRead_btn': function() { 
     Router.go('Modeles');
   } 
 });
 
-Template.readFormModelesTemplate.helpers({
-marqueOptions: function () {
-  return Marques.find().map(function (m) {
-    return {label: m.nom, value: m._id};
-  });
-}
-});
