3b5a407ea46372c1a5e91dbb044d8a68b9589249
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-05-17 13:51:16 +0200

    .

--- client/templates/Accounts-System/dashboardUtilisateursHelpers.js
+++
@@ -1,9 +0,0 @@
-/*Template.dashboardUtilisateursTemplate.helpers({
-    isAdmin() {
-        if(Roles.userIsInRole(Meteor.user(), ['administrator'])){
-            return true;
-        } else {
-            return false; 
-        } 
-    }
-});*/
