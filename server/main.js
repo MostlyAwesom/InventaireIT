946cb7a0180160cba7eef70f5b5a73057248c5d8
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-06-19 14:15:42 +0200

    .

--- server/main.js
+++ server/main.js
@@ -1,12 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
- // SSLProxy({
-//		port: 6000, //or 443 (normal port/requires sudo)
-//		ssl: {
-//			key: Assets.getText("server.key"),
-//			cert: Assets.getText("server.crt")
-//		}
-//	});
-//	SSL('/home/egremyb/Dropbox/InventaireIT/private/server.key', '/home/egremyb/Dropbox/InventaireIT/private/server.crt');
+ SSLProxy({
+		port: 6000, //or 443 (normal port/requires sudo)
+		ssl: {
+			key: Assets.getText("server.key"),
+			cert: Assets.getText("server.crt")
+		}
+	});
+	SSL('/home/egremyb/Dropbox/InventaireIT/private/server.key', '/home/egremyb/Dropbox/InventaireIT/private/server.crt');
 });
