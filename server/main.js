b13aa4e79d457529b322f82642be890b5b45dd5f
MostlyAwesom <32512987+MostlyAwesom@users.noreply.github.com>
2018-06-19 14:21:49 +0200

    .

--- server/main.js
+++ server/main.js
@@ -8,5 +8,5 @@ Meteor.startup(() => {
 			cert: Assets.getText("server.crt")
 		}
 	});
-	SSL('~/git/InventaireIT/private/server.key', '~/git/InventaireIT/private/server.crt');
+	SSL('/git/InventaireIT/private/server.key', '/git/InventaireIT/private/server.crt');
 });
